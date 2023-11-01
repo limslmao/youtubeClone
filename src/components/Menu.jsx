5001500be1a9b9ea78a127a272a972e4e2c5ff86
limslmao <97270285+limslmao@users.noreply.github.com>
2023-11-01 12:52:56 +0800

    create Menu wrapper

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,8 +3,19 @@ import styled from "styled-components";
 
 const Container = styled.div`
   flex: 1;
+  background-color: #202020;
+  height: 100vh;
+  color: white;
+`;
+
+const Wrapper = styled.div`
+  padding: 18px 26px;
 `;
 
 export const Menu = () => {
-  return <Container>Menu</Container>;
+  return (
+    <Container>
+      <Wrapper>items</Wrapper>
+    </Container>
+  );
 };
