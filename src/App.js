a4ee3ecbed800b8739f0bccb3dabd91105d7a720
limslmao <97270285+limslmao@users.noreply.github.com>
2023-11-01 11:59:00 +0800

    project init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+function App() {
+  return (
+    <Container>
+      {/*menu*/}
+      {/*main*/}
+    </Container>
+  );
+}
+
+export default App;
