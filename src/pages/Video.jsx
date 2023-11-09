9e22991720e44a9a375005901edfcca16b622a57
limslmao <jamesmau87@gmail.com>
2023-11-09 18:25:18 +0800

    add Content Component

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import styled from "styled-components";
+import AddTaskOutlinedIcon from "@mui/icons-material/AddTaskOutlined";
+import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
+import ThumbDownOutlinedIcon from "@mui/icons-material/ThumbDownOutlined";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
 
 const Container = styled.div`
   display: flex;
@@ -10,6 +14,46 @@ const Content = styled.div`
   flex: 5;
 `;
 
+const VideoWrapper = styled.div`
+  flex: 5;
+`;
+
+const Title = styled.h1`
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Details = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Info = styled.span`
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 20px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Button = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
 const Recommendation = styled.div`
   flex: 2;
 `;
@@ -17,7 +61,38 @@ const Recommendation = styled.div`
 const Video = () => {
   return (
     <Container>
-      <Content>content</Content>
+      <Content>
+        <VideoWrapper>
+          <iframe
+            width="100%"
+            height="720"
+            src="https://www.youtube.com/embed/k3Vfj-e1Ma4"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </VideoWrapper>
+        <Title>Test Video</Title>
+        <Details>
+          <Info>7,948,132 views・Jun 22, 2022</Info>
+          <Buttons>
+            <Button>
+              <ThumbUpOutlinedIcon /> Like
+            </Button>
+            <Button>
+              <ThumbDownOutlinedIcon /> Dislike
+            </Button>
+            <Button>
+              <ReplyOutlinedIcon /> Share
+            </Button>
+            <Button>
+              <AddTaskOutlinedIcon /> Save
+            </Button>
+          </Buttons>
+        </Details>
+        <Hr />
+      </Content>
       <Recommendation>recommendation</Recommendation>
     </Container>
   );
