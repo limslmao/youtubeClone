0bfceecb82112d41a2417ac8b4e2433e48354fa6
limslmao <jamesmau87@gmail.com>
2023-11-09 17:24:57 +0800

    add card image and link

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -17,6 +17,7 @@ import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   flex: 1;
@@ -83,10 +84,12 @@ export const Menu = ({ darkMode, setDarkMode }) => {
   return (
     <Container>
       <Wrapper>
-        <Logo>
-          <Img src={LimTube} />
-          LimTube
-        </Logo>
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <Logo>
+            <Img src={LimTube} />
+            LimTube
+          </Logo>
+        </Link>
         <Item>
           <HomeIcon />
           Home
